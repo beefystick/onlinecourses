c926a5f6ca6115f85f91ab0f2547e8f027eb1112
beefystick <gai.sahad@gmail.com>
2023-05-09 17:27:39 +0200

    Local to Git

---
+++ onlinecourse/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from .models import Course, Lesson, Instructor, Learner, Question, Choice
+
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 5
+
+class QuestionInline(admin.StackedInline):
+    model = Choice
+    extra = 5
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonInline]
+    list_display = ('name', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['name', 'description']
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Question)
+admin.site.register(Choice)
+admin.site.register(Instructor)
+admin.site.register(Learner)
